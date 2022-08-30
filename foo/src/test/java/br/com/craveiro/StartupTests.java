c88611a28cf6c1427ecb4932e90d252543c19922
antoniocraveiro <antonio.craveirojr@gmail.com>
2022-08-30 13:27:09 -0300

    Initial commit

---
+++ foo/src/test/java/br/com/craveiro/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.craveiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
